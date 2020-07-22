2ef148c06e4c888459bf30d16f9bb0522edf205a
Minminhfut <46999146+Minminhfut@users.noreply.github.com>
2020-07-22 19:19:32 +0800

    Update Enrich_Intra_Domain.m

--- Enrich_Intra_Domain.m
+++ Enrich_Intra_Domain.m
@@ -4,9 +4,9 @@ function [Ws,Wt] = Enrich_Intra_Domain(Sshare,Sspecial,Tshare,Tspecial,parameter
 % Tshare:Ncxd
 % Tspecial:Ntxd
 % parameter
-[Nc,d] = size(Sshare);
-Ws = inv(Sshare*Sshare' + parameter.rho*diag(ones(Nc,1))) *(Sshare*Sspecial');
-Wt = inv(Tshare*Tshare' + parameter.rho*diag(ones(Nc,1))) *(Tshare*Tspecial');
-Ws = Ws';
-Wt = Wt';
+    [Nc,d] = size(Sshare);
+    Ws = inv(Sshare*Sshare' + parameter.rho*diag(ones(Nc,1))) *(Sshare*Sspecial');
+    Wt = inv(Tshare*Tshare' + parameter.rho*diag(ones(Nc,1))) *(Tshare*Tspecial');
+    Ws = Ws';
+    Wt = Wt';
 end
